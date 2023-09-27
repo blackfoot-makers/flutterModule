25ce91092e2657642054e24a0398236633930e9d
Thing-leoh Nicolas <nico@blackfoot.io>
2023-09-27 10:52:03 +0200

    feat: :sparkles: add firebase crashlytics to app with android config

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:workshops/cart_provider.dart';
-import 'package:workshops/product_model.dart';
+import 'package:workshops/firebase_options.dart';
 import 'package:workshops/profile_screen.dart';
 import 'package:workshops/shop_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
