1ca81ec336046038661c22fd3871034f35665ad1
Thing-leoh Nicolas <nico@blackfoot.io>
2024-03-01 16:19:57 +0100

    chore(2024): :rotating_light: update documentation for lint error

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:workshops/main.dart' as app;
+import 'package:workshop_epitech/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
